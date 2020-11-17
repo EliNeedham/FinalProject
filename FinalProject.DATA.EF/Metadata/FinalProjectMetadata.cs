42af877884a5731d20547e7825c6087a8b93d188
EliNeedham <EliNeedham@outlook.com>
2020-11-17 11:59:47 -0600

    started doing metadata realized having a description of the service would be a good idea so changed the db and will update edmx again

--- FinalProject.DATA.EF/Metadata/FinalProjectMetadata.cs
+++ FinalProject.DATA.EF/Metadata/FinalProjectMetadata.cs
@@ -9,24 +9,46 @@ namespace FinalProject.DATA.EF//.Metadata
 {
     public class UserDetailsMetadata 
     {
-        [Display(Name = "User Id")]
-        public string UserId { get; set; }
+        //public string UserId { get; set; }
 
+        [Display(Name = "Company Name")]
+        [StringLength(100, ErrorMessage = " *Please do not exceed 100 characters :)")]
         public string CompanyName { get; set; }
 
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "*Please do not exceed 50 characters.")]
+        [Required(ErrorMessage = " * First name is required.")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "*Please do not exceed 50 characters.")]
+        [Required(ErrorMessage = " * Last name is required")]
         public string LastName { get; set; }
 
-        public string Address { get; set; }
-
-        public string City { get; set; }
-
-        public string State { get; set; }
-
-        public string ZipCode { get; set; }
-
+        [Display(Name = "Profile Picture")]
         public string UserPhoto { get; set; }
 
     }
+
+    [MetadataType(typeof(UserDetailsMetadata))]
+    public partial class UserDetails
+    {
+        [Display(Name = "Name")]
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
+
+    public class ServicesMetadata
+    {
+        //public int ServiceId { get; set; }
+
+        [Display(Name = "Service")]
+        [Required(ErrorMessage = " *Type of service is required")]
+        public string ServiceType { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        [Required(ErrorMessage = " *Cost of service is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "* price must be a valid number, 0 or larger")]
+        public decimal Price { get; set; }
+    }
+
 }
--- FinalProject.MVC.UI/App_Start/BundleConfig.cs
+++ FinalProject.MVC.UI/App_Start/BundleConfig.cs
@@ -8,7 +8,19 @@ namespace FinalProject.MVC.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+ 
+                        "~/Content/assets/vendor/jquery/jquery.min.js",
+                        "~/Content/assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                        "~/Content/assets/vendor/jquery.easing/jquery.easing.min.js",
+                        "~/Content/assets/vendor/php-email-form/validate.js",
+                        "~/Content/assets/vendor/jquery-sticky/jquery.sticky.js",
+                        "~/Content/assets/vendor/isotope-layout/isotope.pkgd.min.js",
+                        "~/Content/assets/vendor/venobox/venobox.min.js",
+                        "~/Content/assets/vendor/waypoints/jquery.waypoints.min.js",
+                        "~/Content/assets/vendor/owl.carousel/owl.carousel.min.js",
+                        "~/Content/assets/vendor/aos/aos.js",
+                        "~/Content/assets/js/main.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,8 +35,16 @@ namespace FinalProject.MVC.UI
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/assets/vendor/bootstrap/css/bootstrap.min.css",
+                "~/Content/assets/vendor/icofont/icofont.min.css",
+                "~/Content/assets/vendor/boxicons/css/boxicons.min.css",
+                "~/Content/assets/vendor/animate.css/animate.min.css",
+                "~/Content/assets/vendor/venobox/venobox.css",
+                "~/Content/assets/vendor/owl.carousel/assets/owl.carousel.min.css",
+                "~/Content/assets/vendor/aos/aos.css",
+                "~/Content/assets/vendor/remixicon/remixicon.css",
+                "~/Content/assets/css/style.css"
+                ));
         }
     }
 }
