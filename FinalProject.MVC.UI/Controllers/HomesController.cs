117bc7ab067a44c602168f83edb769f0bef97c8b
EliNeedham <EliNeedham@outlook.com>
2020-11-18 12:10:01 -0600

    added upload utility, but it isn't working.....

--- FinalProject.MVC.UI/Controllers/HomesController.cs
+++ FinalProject.MVC.UI/Controllers/HomesController.cs
@@ -7,6 +7,9 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using FinalProject.DATA.EF;
+using FinalProject.MVC.UI.Utilities;
+using FinalProject.MVC.UI.Models;
+using System.Drawing;
 
 namespace FinalProject.MVC.UI.Controllers
 {
@@ -48,10 +51,51 @@ namespace FinalProject.MVC.UI.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "HomeId,HomeName,Address,City,State,ZipCode,OwnderId,HomePhoto,SpecialNotes,IsActive,DateAdded")] Homes homes)
+        public ActionResult Create([Bind(Include = "HomeId,HomeName,Address,City,State,ZipCode,OwnderId,HomePhoto,SpecialNotes,IsActive,DateAdded")] Homes homes, HttpPostedFileBase homePhoto)
         {
             if (ModelState.IsValid)
             {
+
+                #region File Upload
+                string file = "noimage.png"; //for this to work in our image files there is a NoImage.png file
+
+                if (homePhoto != null)
+                {
+                    file = homePhoto.FileName;
+                    //we need to make sure they are actually uploading an appropriate file type
+                    string ext = file.Substring(file.LastIndexOf('.'));
+                    string[] goodExts = { ".jpeg", ".jpg", ".png", ".gif" };
+                    //check that the uploaded file is in our list of good file extensions
+                    if (goodExts.Contains(ext))
+                    {
+                        //if valid ext, check file size <= 4mb (max by default from ASP.net)
+                        if (homePhoto.ContentLength <= 4194304) // specifying in bytes how big file can be
+                        {
+                            //create a new file name using a guid - a lot of users probably have images with the same names so we change it from what the user had to a guid Globally Unique Identifier
+                            file = Guid.NewGuid() + ext;
+
+                            #region Resize Image
+                            string savePath = Server.MapPath("~/Content/assets/img/Uploads/");
+
+                            //taking the contents of this file and creatign a stream of bytes, http file base type is becmonig a stream of bytes into a type of image. this conversion has to take place for us to be able to resize the image
+                            Image convertedImage = Image.FromStream(homePhoto.InputStream);
+
+                            int maxImageSize = 500;
+
+                            int maxThumbSize = 100;
+                            //if you allowed image uploads for magazine and books - you would need to repeat that code - that's why the image service code is in an imageservice area
+
+                            UploadUtility.ResizeImage(savePath, file, convertedImage, maxImageSize, maxThumbSize);
+                            //saves image onto server - but doesn't update db need to make sure to update what is stored in the db
+                            #endregion
+                        }
+                    }
+
+
+                }
+                homes.HomePhoto = file;
+                #endregion
+
                 db.Homes.Add(homes);
                 db.SaveChanges();
                 return RedirectToAction("Index");
